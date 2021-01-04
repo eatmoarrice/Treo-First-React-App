a0943af5a7da0cc8cecbbed5fafddb4b48d26398
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2021-01-04 07:18:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Card, CardDeck } from 'react-bootstrap';
+
+const Fruit = ({ description, name, emoji, price }) => {
+	return (
+		<Card style={{ width: '18rem' }}>
+			<Card.Body>
+				<Card.Title>
+					{name}
+					{emoji}
+				</Card.Title>
+				<Card.Text>{description}</Card.Text>
+				<Card.Text>Price: ${price}/kg</Card.Text>
+				<Card.Text>{description}</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const fruits = [
+		{ description: 'Good.', name: 'apple', emoji: '🍎', price: 0.5 },
+		{ description: 'So so.', name: 'orange', emoji: '🍊', price: 0.2 },
+		{ description: 'Okay.', name: 'grape', emoji: '🍇', price: 1.2 },
+		{ description: 'Excellent', name: 'pear', emoji: '🍐', price: 1.5 },
+	];
+	return (
+		<Container>
+			<CardDeck>
+				{fruits.map((fruit, i) => (
+					<Fruit name={fruit.name} description={fruit.description} emoji={fruit.emoji} price={fruit.price} key={i} />
+				))}
+			</CardDeck>
+		</Container>
+	);
 }
 
 export default App;
